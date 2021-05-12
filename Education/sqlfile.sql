21a2ae7fa7a74324b66df6fa87dc8cc44a32f0fb
Josh Weidenaar <josh.weidenaar@intersectillinois.org>
2021-05-11 19:57:03 -0500

    added to John's SQL Schema and added my ipynb which pulls from World Bank API

--- Education/sqlfile.sql
+++ Education/sqlfile.sql
@@ -1,6 +1,10 @@
 -- run this code BEFORE jupyterlab
 drop table if exists happiness;
 drop table if exists education;
+drop table if exists country_information;
+drop table if exists gender;
+drop table if exists series_information
+
 -- Create tables for raw data to be loaded into
 CREATE TABLE happiness (
 id INT PRIMARY KEY,
@@ -15,6 +19,31 @@ country TEXT,
 percent_educated FLOAT,
 country_code INT PRIMARY KEY
 );
+
+
+CREATE TABLE country_information (
+country_value TEXT,
+country_isocode varchar(3),
+country_id varchar(2) PRIMARY KEY
+);
+
+CREATE TABLE series_information (
+series_id varchar(17) PRIMARY KEY,
+series_name TEXT
+);
+
+CREATE TABLE gender (   
+country_id varchar(2),
+val_date Date,
+value float,
+decimal INT,
+PRIMARY KEY(country_id),
+   CONSTRAINT fk_country
+      FOREIGN KEY(country_id) 
+	  REFERENCES country_information(country_id)
+      FOREIGN KEY(series_id)
+      REFERENCES series_information(series_id)
+)
 -- Run this code after running jupyter file
 SELECT happiness.id, happiness.country, happiness.score, education.percent_educated
 FROM happiness
